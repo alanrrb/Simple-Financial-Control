91c008fe7d42aafbe6051b6becc0e34642439d9a
Alan Rafael <alanrrb@gmail.com>
2010-03-22 00:11:49 -0300

    refactoring no saldo_controller

--- app/controllers/saldo_controller.rb
+++ app/controllers/saldo_controller.rb
@@ -1,24 +1,12 @@
 class SaldoController < ApplicationController
   def atual
-    if params[:ano] and params[:mes] and params[:dia]
-      ano, mes, dia = params[:ano].to_i, params[:mes].to_i, params[:dia].to_i
-      data_saldo = Date.new ano, mes, dia
-    else
-      aux = DateTime.now
-      data_saldo =  Date.new(aux.year, aux.month, aux.day)
-    end
+    criterios = "conta_id = ? AND data <= ? AND credito_debito = ?"
     
     credito = Lancamento.sum :valor, :conditions => 
-    ["conta_id = ? AND data <= ? AND credito_debito = ?", 
-      params[:id], 
-      data_saldo, 
-      'c']
+    [criterios, params[:id], data_saldo, 'c']
     
     debito  = Lancamento.sum :valor, :conditions => 
-      ["conta_id = ? AND data <= ? AND credito_debito = ?", 
-        params[:id], 
-        data_saldo, 
-        'd']
+      [criterios, params[:id], data_saldo, 'd']
     
     @conta = Conta.find_by_id(params[:id])
     @saldo = @conta.valor_inicial + (credito - debito)
@@ -28,7 +16,14 @@ class SaldoController < ApplicationController
     end
   end
   
-  def data_saldo params
-  
+private
+  def data_saldo
+    if params[:ano] and params[:mes] and params[:dia]
+      ano, mes, dia = params[:ano].to_i, params[:mes].to_i, params[:dia].to_i
+      data_saldo = Date.new ano, mes, dia
+    else
+      aux = DateTime.now
+      data_saldo =  Date.new(aux.year, aux.month, aux.day)
+    end
   end
 end
